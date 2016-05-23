5cf4a968da2d139b0d55fe5dc22b23e59e22d532
Riim <riim@yandex.ru>
2016-05-23 12:22:54 +0300

    update cellx

--- dist/Rista.js
+++ dist/Rista.js
@@ -2084,7 +2084,11 @@ return /******/ (function(modules) { // webpackBootstrap
 			var cell = (queue || []).shift();
 
 			if (!cell) {
-				queue = releasePlan[++releasePlanIndex];
+				if (++releasePlanIndex > releasePlanToIndex) {
+					break;
+				}
+
+				queue = releasePlan[releasePlanIndex];
 				continue;
 			}
 
