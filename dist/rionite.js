9cc59fb32637a264b9166967514ca586f2c991d3
Riim <riim@yandex.ru>
2017-01-08 02:17:27 +0300

    update beml

--- dist/rionite.js
+++ dist/rionite.js
@@ -3238,46 +3238,33 @@ var renderAttributes_1 = __webpack_require__(46);
 var elDelimiter = '__';
 var Template = (function () {
     function Template(beml, opts) {
+        var block = new Parser_1.default(beml).parse();
+        var blockName = opts && opts.blockName || block.name;
+        if (!blockName) {
+            throw new TypeError('blockName is required');
+        }
         var parent = this.parent = opts && opts.parent || null;
-        if (beml !== undefined) {
-            var block = new Parser_1.default(beml).parse();
-            var blockName = opts && opts.blockName || block.name;
-            if (!blockName) {
-                throw new TypeError('blockName is required');
+        this._elementClassesTemplate = parent ?
+            [blockName + elDelimiter].concat(parent._elementClassesTemplate) :
+            [blockName + elDelimiter, ''];
+        this._nodes = [(this._currentNode = { elementName: null, source: null, innerSource: [], hasSuperCall: false })];
+        var nodeMap = this._nodeMap = {};
+        block.content.forEach(this._handleNode, this);
+        this._renderer = parent ?
+            parent._renderer :
+            Function("return " + this._currentNode.innerSource.join(' + ') + ";");
+        Object.keys(nodeMap).forEach(function (name) {
+            var node = nodeMap[name];
+            this[name] = Function("return " + node.source.join(' + ') + ";");
+            if (node.hasSuperCall) {
+                var inner_1 = Function('$super', "return " + node.innerSource.join(' + ') + ";");
+                var parentElementRenderer_1 = parent && parent._elementRendererMap[name + '@inner'];
+                this[name + '@inner'] = function () { return inner_1.call(this, parentElementRenderer_1); };
             }
-            this._elementClassesTemplate = parent ?
-                [blockName + elDelimiter].concat(parent._elementClassesTemplate) :
-                [blockName + elDelimiter, ''];
-            this._nodes = [(this._currentNode = { elementName: null, source: [], hasSuperCall: false })];
-            var nodeMap_1 = this._nodeMap = {};
-            block.content.forEach(this._handleNode, this);
-            this._renderer = parent ?
-                parent._renderer :
-                Function("return [" + this._currentNode.source.join(', ') + "].join('');");
-            Object.keys(nodeMap_1).forEach(function (name) {
-                var node = nodeMap_1[name];
-                if (node.hasSuperCall) {
-                    var inner_1 = Function('$super', "return " + node.source.join(' + ') + ";");
-                    var parentElementRenderer_1 = parent && parent._elementRendererMap[name];
-                    this[name] = function () { return inner_1.call(this, parentElementRenderer_1); };
-                }
-                else {
-                    this[name] = Function("return " + node.source.join(' + ') + ";");
-                }
-            }, (this._elementRendererMap = Object.create(parent && parent._elementRendererMap)));
-        }
-        else {
-            var blockName = opts && opts.blockName;
-            if (!blockName) {
-                throw new TypeError('blockName is required');
+            else {
+                this[name + '@inner'] = Function("return " + node.innerSource.join(' + ') + ";");
             }
-            if (!parent) {
-                throw new TypeError('parent is required if beml is not defined');
-            }
-            this._elementClassesTemplate = [blockName + elDelimiter].concat(parent._elementClassesTemplate);
-            this._renderer = parent._renderer;
-            this._elementRendererMap = parent._elementRendererMap;
-        }
+        }, (this._elementRendererMap = Object.create(parent && parent._elementRendererMap)));
     }
     Template.prototype._handleNode = function (node) {
         switch (node.nodeType) {
@@ -3287,32 +3274,44 @@ var Template = (function () {
                 var tagName = el.tagName;
                 var elName = el.name;
                 var content = el.content;
+                var hasContent = content && content.length;
                 if (elName) {
-                    var currentNode = { elementName: elName, source: [], hasSuperCall: false };
+                    var currentNode = {
+                        elementName: elName,
+                        source: [
+                            "'<" + tagName + renderAttributes_1.default(this._elementClassesTemplate, el) + ">'",
+                            hasContent ?
+                                "this['" + elName + "@inner']() + '</" + tagName + ">'" :
+                                (tagName in selfClosingTags_1.default ? "''" : "'</" + tagName + ">'")
+                        ],
+                        innerSource: [],
+                        hasSuperCall: false
+                    };
                     nodes.push((this._currentNode = currentNode));
                     this._nodeMap[elName] = currentNode;
                 }
-                this._currentNode.source.push("'<" + tagName + renderAttributes_1.default(this._elementClassesTemplate, el) + ">'");
-                var hasContent = content && content.length;
+                else {
+                    this._currentNode.innerSource.push("'<" + tagName + renderAttributes_1.default(this._elementClassesTemplate, el) + ">'");
+                }
                 if (hasContent) {
                     content.forEach(this._handleNode, this);
-                }
-                if (hasContent || !(tagName in selfClosingTags_1.default)) {
-                    this._currentNode.source.push("'</" + tagName + ">'");
                 }
                 if (elName) {
                     nodes.pop();
                     this._currentNode = nodes[nodes.length - 1];
-                    this._currentNode.source.push("this['" + elName + "']()");
+                    this._currentNode.innerSource.push("this['" + elName + "']()");
+                }
+                else if (hasContent || !(tagName in selfClosingTags_1.default)) {
+                    this._currentNode.innerSource.push("'</" + tagName + ">'");
                 }
                 break;
             }
             case Parser_1.NodeType.TEXT: {
-                this._currentNode.source.push("'" + escape_string_1.default(node.value) + "'");
+                this._currentNode.innerSource.push("'" + escape_string_1.default(node.value) + "'");
                 break;
             }
             case Parser_1.NodeType.SUPER_CALL: {
-                this._currentNode.source.push("$super.call(this)");
+                this._currentNode.innerSource.push("$super.call(this)");
                 this._currentNode.hasSuperCall = true;
                 break;
             }
