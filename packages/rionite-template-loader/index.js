7b2422a07eac7558075e035f994a80bdaab51819
Dmitry Vibe <riim@yandex.ru>
2019-11-22 17:31:09 +0300

    rionite-template-loader: use custom stringify

--- packages/rionite-template-loader/index.js
+++ packages/rionite-template-loader/index.js
@@ -1,7 +1,25 @@
 const { TemplateParser } = require('@riim/rionite-template-parser-2');
 
+function stringify(value) {
+	if (value === undefined) {
+		return '';
+	}
+
+	if (Array.isArray(value)) {
+		let source = '';
+
+		for (let i = 0, l = value.length; i < l; ++i) {
+			source += (i ? ',' : '') + stringify(value[i]);
+		}
+
+		return `[${source}]`;
+	}
+
+	return JSON.stringify(value);
+}
+
 module.exports = function(content) {
-	return `const template = ${JSON.stringify(
+	return `const template = ${stringify(
 		new TemplateParser(content).parse()
 	)};\nexport default template;`;
 };
